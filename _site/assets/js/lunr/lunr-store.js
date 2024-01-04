5c47ede1bdcb60481fa4908c038de4446a11fc3a
jihyun-log <o.ov0227@gmail.com>
2024-01-04 23:09:56 +0900

    update Initial_Posting

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
-        "title": "Initial_posting",
-        "excerpt":" ","categories": [],
-        "tags": [],
+        "title": "github.io 블로그 첫 포스팅입니다",
+        "excerpt":"제목, 문단별 제목을 쓰고 싶을 때 제목 1단계 제목 2단계 제목 3단계 제목 4단계 제목 5단계 제목 6단계 수평선 사용하고 싶을 때 3개 연달아 쓰기 List 형태로 사용하고 싶을 때 첫번째 두번째 세번째 순서없음 이렇게 가능합니다 신기하죠? 강조하고 싶을 때 볼드(진하게) 이탤릭체(기울여서) 취소선 밑줄 볼드로 진하게 만들다가이탤릭으로 기울이고다시 볼드로(중복 활용도...","categories": [],
+        "tags": ["Blog"],
         "url": "/Initial_Posting/",
         "teaser": null
       }]
